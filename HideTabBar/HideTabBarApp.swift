39f9c7bcddb6b1f15aa9749b219eda506cd5e865
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-18 22:28:02 +0200

    Initial Commit

---
+++ HideTabBar/HideTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideTabBarApp.swift
+//  HideTabBar
+//
+//  Created by Michele Manniello on 18/08/21.
+//
+
+import SwiftUI
+
+@main
+struct HideTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
